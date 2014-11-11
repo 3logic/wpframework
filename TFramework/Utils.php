224f3faad6fbdc6a12dba9c59390e4d88da68193
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2014-11-11 17:50:05 +0100

    Corretta implementazione

--- TFramework/Utils.php
+++ TFramework/Utils.php
@@ -62,11 +62,15 @@ class Utils{
         return self::$_sites;
      }
 
+     /**
+      * Ritorna true se ACF è attivo sul sito
+      * @return boolean 
+      */
      public static function is_acf_active(){
-        if(!is_admin()){
-            include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-        }
-        return is_plugin_active( 'advanced-custom_fields/acf.php' );
+        if(!function_exists("register_field_group"))
+            return false;
+        include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+        return \is_plugin_active( 'advanced-custom_fields/acf.php' );
      }
 
      
